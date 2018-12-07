9a05199add01c9781fa7ab2a93ca618b82542fe8
whjin <1414848321@qq.com>
2018-12-07 13:24:39 +0800

    push code

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: whjin
+ * Date: 2018/12/7
+ * Time: 13:21
+ */
+$view_config = array('left_delimiter' => '{', 'right_delimiter' => '}', 'template_dir' => 'tpl', 'compile_dir' => 'template_c');
--- index.php
+++ index.php
@@ -6,4 +6,9 @@
  * Time: 0:47
  */
 require_once('function.php');
+require_once('config.php');
 
+$view = ORG('smarty/', 'Smarty', $view_config);
+$controller = $_GET['controller'];
+$method = $_GET['method'];
+C($controller, $method);
